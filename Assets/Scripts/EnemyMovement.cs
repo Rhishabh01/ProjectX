539199c695194c1031928746776a1f900bcb175a
Rhishabh01 <rhishabh89@gmail.com>
2026-02-06 19:57:41 +0530

    LimitMove

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -17,17 +17,36 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
 
-        Vector3 lookDirect=( player.transform.localPosition).normalized;
-       // enemyRb.transform.Translate(Rot * enemySpeed * Time.deltaTime);*/
 
-       Vector3 Rot = (player.transform.position);
-        gameObject.transform.LookAt(Rot);
+        Vector3 Distance = (player.transform.position - enemyRb.transform.position);
+        Vector3 lookDirect = (player.transform.localPosition).normalized;
+        Vector3 Rot = (player.transform.position);
+
+        gameObject.transform.LookAt(Rot, Vector3.up);
         enemyRb.transform.Translate(-lookDirect * enemySpeed * Time.deltaTime);
-        
 
+
+        Rayhit = Physics.SphereCast(enemyRb.transform.position, 5, Vector3.up,out RaycastHit hitInfo);
         
+        if(Rayhit) 
+        {
+            gameObject.transform.LookAt(Rot, Vector3.up);
+            enemyRb.transform.Translate(-lookDirect * enemySpeed * Time.deltaTime);
+        }
+        else
+        {
+            gameObject.transform.LookAt(Rot, Vector3.up);
+        }
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(-10);
+        }
+    }
+
 }
